f1c7fb0e00ab3395c265ccff25fe145bb7fb24ff
JYCabello <j.cabello.ramos@gmail.com>
2020-12-16 19:49:22 +0100

    Created migration, field nullable

--- EntityFrameworkExamples/EntityFrameworkExamples/Migrations/20201216184352_NullableZucchini.Designer.cs
+++ EntityFrameworkExamples/EntityFrameworkExamples/Migrations/20201216184352_NullableZucchini.Designer.cs
@@ -3,14 +3,16 @@ using EntityFrameworkExamples;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFrameworkExamples.Migrations
 {
     [DbContext(typeof(FruitsDbContext))]
-    partial class FruitsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201216184352_NullableZucchini")]
+    partial class NullableZucchini
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EntityFrameworkExamples/EntityFrameworkExamples/Migrations/20201216184352_NullableZucchini.cs
@@ -0,0 +1,27 @@
+﻿namespace EntityFrameworkExamples.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class NullableZucchini : Migration
+    {
+        protected override void Down(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.AlterColumn<string>(
+                "Zucchini",
+                "FruitBaskets",
+                "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+        protected override void Up(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.AlterColumn<string>(
+                "Zucchini",
+                "FruitBaskets",
+                "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+    }
+}
