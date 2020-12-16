d7b494e4316067f4afb92229b2e5ad8049dfa0a1
JYCabello <j.cabello.ramos@gmail.com>
2020-12-16 19:55:26 +0100

    Migration to remove the field.

---
+++ EntityFrameworkExamples/EntityFrameworkExamples/Migrations/20201216185234_RemoveZucchini.cs
@@ -0,0 +1,13 @@
+﻿namespace EntityFrameworkExamples.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class RemoveZucchini : Migration
+    {
+        protected override void Down(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.AddColumn<string>("Zucchini", "FruitBaskets", "nvarchar(max)", nullable: true);
+
+        protected override void Up(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.DropColumn("Zucchini", "FruitBaskets");
+    }
+}
