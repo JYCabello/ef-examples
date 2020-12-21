5f98151e2687f304ec06e1357a92285932b570b5
JYCabello <j.cabello.ramos@gmail.com>
2020-12-21 13:23:06 +0100

    Cleanup

--- EntityFrameworkExamples/EntityFrameworkExamples/Models/BasketMaker.cs
+++ EntityFrameworkExamples/EntityFrameworkExamples/Models/BasketMaker.cs
@@ -1,15 +1,10 @@
 ﻿namespace EntityFrameworkExamples.Models
 {
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
 
     public class BasketMaker
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
-
         public List<FruitBasket> FruitBaskets { get; set; } = new();
+        public int ID { get; set; }
     }
 }
--- EntityFrameworkExamples/EntityFrameworkExamples/Models/FruitBasket.cs
+++ EntityFrameworkExamples/EntityFrameworkExamples/Models/FruitBasket.cs
@@ -1,8 +1,6 @@
 ﻿namespace EntityFrameworkExamples.Models
 {
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
 
     public class FruitBasket
     {
@@ -10,8 +8,6 @@
 
         public List<BasketMaker> BasketMakers { get; set; } = new();
 
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
         public string Pear { get; set; } = string.Empty;
